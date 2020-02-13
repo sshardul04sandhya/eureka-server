4730a693231c6fd1770102b5ca76a6cd6ec74d15
sshardul04sandhya <sshardul.04sandhya@gmail.com>
2020-02-13 12:11:33 +0530

    configuration changes done

--- commerce-product-details/src/main/java/com/example/ecommerce/commerceproductdetails/controller/ProductController.java
+++ commerce-product-details/src/main/java/com/example/ecommerce/commerceproductdetails/controller/ProductController.java
@@ -1,12 +1,8 @@
 package com.example.ecommerce.commerceproductdetails.controller;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.ws.rs.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.example.ecommerce.commerceproductdetails.exception.ProductNotFoundException;
 import com.example.ecommerce.commerceproductdetails.model.ProductEntity;
 import com.example.ecommerce.commerceproductdetails.service.ProductService;
 
@@ -33,7 +27,6 @@ public class ProductController {
 
 	@GetMapping
     public ResponseEntity<List<ProductEntity>> getProductList() {
-		//List<ProductEntity> pe = productService.getProductList();
 		LOGGER.info("get list of Product::"+productService.getProductList());
         return new ResponseEntity<List<ProductEntity>>(productService.getProductList(), new HttpHeaders(), HttpStatus.OK);
      
@@ -42,7 +35,6 @@ public class ProductController {
 	@GetMapping("/productCode/{product_code}")
     public ResponseEntity<ProductEntity> getProductListByProductCode(@PathVariable("product_code") Long product_code)
      {
-		//ProductEntity pList = productService.getProductByCode(product_code);
 		LOGGER.info("get Product based on productCode::"+productService.getProductByCode(product_code));
 		return new ResponseEntity<ProductEntity>(productService.getProductByCode(product_code),new HttpHeaders(), HttpStatus.OK);
      
@@ -51,7 +43,6 @@ public class ProductController {
 	@GetMapping("/brandName/{brandName}")
     public ResponseEntity<List<ProductEntity>> getProductListByBrandNameCode(@PathVariable("brandName") String brandName)
     {
-		//List<ProductEntity> pList = productService.getProductByBrandName(brandName);
 		LOGGER.info("get Product based on brandName::"+productService.getProductByBrandName(brandName));
 	   return new ResponseEntity<List<ProductEntity>>(productService.getProductByBrandName(brandName),new HttpHeaders(), HttpStatus.OK);
      
@@ -60,7 +51,6 @@ public class ProductController {
 	@GetMapping("/productName/{productName}")
     public ResponseEntity<List<ProductEntity>> getProductListByProductName(@PathVariable("productName") String productName)
      {
-		//List<ProductEntity> pList = productService.getProductByProductName(productName);
 		LOGGER.info("get Product based on brandName::"+productService.getProductByProductName(productName));
 	   return new ResponseEntity<List<ProductEntity>>(productService.getProductByProductName(productName),new HttpHeaders(), HttpStatus.OK);
      
--- commerce-product-details/src/main/java/com/example/ecommerce/commerceproductdetails/repository/ProductRepository.java
+++ commerce-product-details/src/main/java/com/example/ecommerce/commerceproductdetails/repository/ProductRepository.java
@@ -1,12 +1,7 @@
 package com.example.ecommerce.commerceproductdetails.repository;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.apache.commons.math.stat.descriptive.summary.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.ecommerce.commerceproductdetails.model.ProductEntity;
